49b1397c514e2c73cc1a5b859ad6d535d7244279
ROHIT CHAND <100352296+rohitchand3141@users.noreply.github.com>
2023-03-23 12:34:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
+function calculateMinCost() 
+{
+	const repoLengthsStr=document.getElementById('rope-lengths').value;
+	const repoLengthsArr=ropeLengthsStr.split(',').map(Number);
+	const n=ropeLengthsArr.length;
+
+	let pq=[];
+	for(let i=0;i<n;i++)
+		{
+			pq.push(ropeLengthsArr[i]);
+		}
+	pq.sort(function(a,b){
+		return a-b;
+	});
+	let res=0;
+	while(pq.length>1)
+	{
+		let first=pq.shift();
+		let second=pq.shift();
+		res=res+first+second;
+		pq.push(first+second);
+		pq.sort(function(a,b){
+			return a-b;
+		});
+	}
+  document.getElementById('result').textContent=res;
 }  
